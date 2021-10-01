8de1a18fde0ac944c34c37a3578547574191547e
aakash404 <aakashkumbhare@gmail.com>
2021-10-01 22:46:57 +0530

    Initial commit

---
+++ src/main/java/com/project/eventlistner/EventlistnerApplication.java
@@ -0,0 +1,13 @@
+package com.project.eventlistner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventlistnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventlistnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/eventlistner/EventlistnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.eventlistner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventlistnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
